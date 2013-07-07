68e059428737699422dba6f666215807816b2b76
omerlevy <omerlevy@gmail.com>
2013-07-07 14:36:48 +0300

    - Added DKPro Similarity dependencies
    - Added EsaLexicalResource v1
    - Added EsaLexicalResourceTest

--- src/main/java/lexent/resource/esa/EsaLexicalResource.java
+++ src/main/java/lexent/resource/esa/EsaLexicalResource.java
@@ -1,9 +1,32 @@
 package lexent.resource.esa;
 
+import java.io.File;
+import java.util.Arrays;
+
 import lexent.resource.LexicalResource;
+import de.tudarmstadt.ukp.similarity.algorithms.api.SimilarityException;
+import de.tudarmstadt.ukp.similarity.algorithms.vsm.InnerVectorProduct;
+import de.tudarmstadt.ukp.similarity.algorithms.vsm.VectorComparator;
+import de.tudarmstadt.ukp.similarity.algorithms.vsm.VectorNorm;
+import de.tudarmstadt.ukp.similarity.algorithms.vsm.store.vectorindex.VectorIndexReader;
 
 public class EsaLexicalResource implements LexicalResource {
-
-	// Fill me
-
+	
+	public EsaLexicalResource() {
+		esa = new VectorComparator(new VectorIndexReader(new File("../DKPRO_HOME/esaIndexesVector/en/wp")));
+		esa.setInnerProduct(InnerVectorProduct.LEFT_DICE);
+		esa.setNormalization(VectorNorm.NONE);
+		// TODO use Lili's sim func (balAPInc) instead
+	}
+	
+	public double probEntails(String t, String h) {
+		try {
+			return esa.getSimilarity(Arrays.asList(t), Arrays.asList(h));
+		} catch (SimilarityException e) {
+			return 0.0;
+		}
+	}
+	
+	private VectorComparator esa;
+	
 }
